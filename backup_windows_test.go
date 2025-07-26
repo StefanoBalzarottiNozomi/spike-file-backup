3d6f32b758c759c8f885906971c5ddb891a3aac7
Stefano Balzarotti <stefano.balzarotti@nozominetworks.com>
2025-07-26 11:15:15 +0100

    add testr with junction and hardlink

--- backup_windows_test.go
+++ backup_windows_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/alecthomas/assert"
+	junction "github.com/nyaosorg/go-windows-junction"
 	"golang.org/x/sys/windows"
 )
 
@@ -38,6 +39,7 @@ func AssertFileExists(t *testing.T, path string) {
 }
 
 func TestCreateFile(t *testing.T) {
+	// Test with real path
 	realRootFolder := t.TempDir()
 	realMidFolder := filepath.Join(realRootFolder, "real_mid_folder")
 	mkdirErr1 := os.Mkdir(realMidFolder, os.ModePerm)
@@ -46,11 +48,49 @@ func TestCreateFile(t *testing.T) {
 	mkdirErr2 := os.Mkdir(realFinalFolder, os.ModePerm)
 	assert.NoError(t, mkdirErr2)
 	realFilePath := filepath.Join(realFinalFolder, "test.txt")
-	fd, ioResult, createErr := CreateFile(realFilePath)
-	assert.NoError(t, createErr)
-	assert.Equal(t, ioResult.Status, windows.STATUS_SUCCESS)
-	assert.NotEqual(t, 0, fd)
-	closeErr := windows.CloseHandle(fd)
+	realFd, realIoResult, realCreateErr := CreateFile(realFilePath)
+	assert.NoError(t, realCreateErr)
+	assert.NotNil(t, realIoResult)
+	assert.NotEqual(t, 0, realFd)
+	closeErr := windows.CloseHandle(realFd)
 	assert.NoError(t, closeErr)
 	AssertFileExists(t, realFilePath)
+
+	// Test with symlink path
+	symlinkRootFolder := t.TempDir()
+	symlinkMidFolder := filepath.Join(symlinkRootFolder, "symlink_mid_folder")
+	symlinkErr := os.Symlink(realMidFolder, symlinkMidFolder)
+	assert.NoError(t, symlinkErr)
+	symlinkFinalFolder := filepath.Join(symlinkMidFolder, "symlink_final_folder")
+	mkdirErr3 := os.Mkdir(symlinkFinalFolder, os.ModePerm)
+	assert.NoError(t, mkdirErr3)
+	symlinkFilePath := filepath.Join(symlinkFinalFolder, "test.txt")
+	_, symLinlIoResult, realCreateErr := CreateFile(symlinkFilePath)
+	assert.Error(t, realCreateErr)
+	assert.NotNil(t, symLinlIoResult)
+
+	// Test with junction path
+	junctionRootFolder := t.TempDir()
+	junctionMidFolder := filepath.Join(junctionRootFolder, "junction_mid_folder")
+	junctionErr := junction.Create(realMidFolder, junctionMidFolder)
+	assert.NoError(t, junctionErr)
+	junctionFinalFolder := filepath.Join(junctionMidFolder, "junction_final_folder")
+	mkdirErr4 := os.Mkdir(junctionFinalFolder, os.ModePerm)
+	assert.NoError(t, mkdirErr4)
+	junctionFilePath := filepath.Join(junctionFinalFolder, "test.txt")
+	_, junctionIoResult, junctionCreateErr := CreateFile(junctionFilePath)
+	assert.Error(t, junctionCreateErr)
+	assert.NotNil(t, junctionIoResult)
+
+	// Test with hardlink path
+	hardlinkRootFolder := t.TempDir()
+	hardlinkFinalFolder := filepath.Join(hardlinkRootFolder, "hardlink_final_folder")
+	hardlinkErr2 := os.Mkdir(hardlinkFinalFolder, os.ModePerm)
+	assert.NoError(t, hardlinkErr2)
+	hardlinkFilePath := filepath.Join(hardlinkFinalFolder, "test.txt")
+	hardlinkErr := os.Link(realFilePath, hardlinkFilePath)
+	assert.NoError(t, hardlinkErr)
+	_, hardlinkIoResult, hardlinkCreateErr := CreateFile(hardlinkFilePath)
+	assert.Error(t, hardlinkCreateErr)
+	assert.NotNil(t, hardlinkIoResult)
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.24.5
 require github.com/alecthomas/assert v1.0.0
 
 require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/alecthomas/colour v0.1.0 // indirect
 	github.com/alecthomas/repr v0.0.0-20210801044451-80ca428c5142 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/nyaosorg/go-windows-junction v0.2.0 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 )
