b5c61de088067bd7ff78633821ca5ca6ee0cd6fb
Stefano Balzarotti <stefano.balzarotti@nozominetworks.com>
2025-09-26 08:45:57 +0100

    add test to simulate the attack

--- backup_windows_test.go
+++ backup_windows_test.go
@@ -1,12 +1,18 @@
 package spikefilebackup
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 
-	"github.com/alecthomas/assert"
 	junction "github.com/nyaosorg/go-windows-junction"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/sys/windows"
 )
 
@@ -30,31 +36,23 @@ func TestConvertPathToNtPath(t *testing.T) {
 	assert.Equal(t, expectedNtPath, ntPath)
 }
 
-func AssertFileExists(t *testing.T, path string) {
-	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		t.Errorf("Expected file to exist at %s, but it does not", path)
-	}
-	assert.NoError(t, err, "File should exist")
-}
-
 func TestCreateFile(t *testing.T) {
 	// Test with real path
 	realRootFolder := t.TempDir()
 	realMidFolder := filepath.Join(realRootFolder, "real_mid_folder")
 	mkdirErr1 := os.Mkdir(realMidFolder, os.ModePerm)
-	assert.NoError(t, mkdirErr1)
+	require.NoError(t, mkdirErr1)
 	realFinalFolder := filepath.Join(realMidFolder, "real_final_folder")
 	mkdirErr2 := os.Mkdir(realFinalFolder, os.ModePerm)
-	assert.NoError(t, mkdirErr2)
+	require.NoError(t, mkdirErr2)
 	realFilePath := filepath.Join(realFinalFolder, "test.txt")
 	realFd, realIoResult, realCreateErr := CreateFile(realFilePath)
 	assert.NoError(t, realCreateErr)
 	assert.NotNil(t, realIoResult)
 	assert.NotEqual(t, 0, realFd)
-	closeErr := windows.CloseHandle(realFd)
-	assert.NoError(t, closeErr)
-	AssertFileExists(t, realFilePath)
+	closeErr := windows.Close(realFd)
+	require.NoError(t, closeErr)
+	assert.FileExists(t, realFilePath)
 
 	// Test with symlink path
 	symlinkRootFolder := t.TempDir()
@@ -65,32 +63,149 @@ func TestCreateFile(t *testing.T) {
 	mkdirErr3 := os.Mkdir(symlinkFinalFolder, os.ModePerm)
 	assert.NoError(t, mkdirErr3)
 	symlinkFilePath := filepath.Join(symlinkFinalFolder, "test.txt")
-	_, symLinlIoResult, realCreateErr := CreateFile(symlinkFilePath)
-	assert.Error(t, realCreateErr)
-	assert.NotNil(t, symLinlIoResult)
+	_, symLinkIoResult, symLinkCreateErr := CreateFile(symlinkFilePath)
+	assert.Error(t, symLinkCreateErr)
+	assert.NotNil(t, symLinkIoResult)
+	assert.NoFileExists(t, symlinkFilePath)
 
 	// Test with junction path
 	junctionRootFolder := t.TempDir()
 	junctionMidFolder := filepath.Join(junctionRootFolder, "junction_mid_folder")
 	junctionErr := junction.Create(realMidFolder, junctionMidFolder)
-	assert.NoError(t, junctionErr)
+	require.NoError(t, junctionErr)
 	junctionFinalFolder := filepath.Join(junctionMidFolder, "junction_final_folder")
 	mkdirErr4 := os.Mkdir(junctionFinalFolder, os.ModePerm)
-	assert.NoError(t, mkdirErr4)
+	require.NoError(t, mkdirErr4)
 	junctionFilePath := filepath.Join(junctionFinalFolder, "test.txt")
 	_, junctionIoResult, junctionCreateErr := CreateFile(junctionFilePath)
-	assert.Error(t, junctionCreateErr)
+	require.Error(t, junctionCreateErr)
 	assert.NotNil(t, junctionIoResult)
+	assert.NoFileExists(t, junctionFilePath)
 
 	// Test with hardlink path
 	hardlinkRootFolder := t.TempDir()
 	hardlinkFinalFolder := filepath.Join(hardlinkRootFolder, "hardlink_final_folder")
 	hardlinkErr2 := os.Mkdir(hardlinkFinalFolder, os.ModePerm)
-	assert.NoError(t, hardlinkErr2)
+	require.NoError(t, hardlinkErr2)
 	hardlinkFilePath := filepath.Join(hardlinkFinalFolder, "test.txt")
 	hardlinkErr := os.Link(realFilePath, hardlinkFilePath)
-	assert.NoError(t, hardlinkErr)
+	require.NoError(t, hardlinkErr)
 	_, hardlinkIoResult, hardlinkCreateErr := CreateFile(hardlinkFilePath)
-	assert.Error(t, hardlinkCreateErr)
+	require.Error(t, hardlinkCreateErr)
 	assert.NotNil(t, hardlinkIoResult)
+}
+
+func TestJunctionRaceAttack(t *testing.T) {
+	rootFolder := t.TempDir()
+	ctx, cancel := context.WithCancel(t.Context())
+	const fileName = "file.txt"
+	maliciousFolder := filepath.Join(rootFolder, "malicious_folder")
+	vulnerableFolder := filepath.Join(rootFolder, "vulnerable_folder")
+	fileInCorrectPath := filepath.Join(vulnerableFolder, fileName)
+	fileInMaliciousPath := filepath.Join(maliciousFolder, fileName)
+
+	assert.NoError(t, os.Mkdir(maliciousFolder, os.ModePerm))
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				_ = os.RemoveAll(vulnerableFolder)
+				_ = os.Mkdir(vulnerableFolder, os.ModePerm)
+				f, _ := os.Create(fileInCorrectPath)
+				_ = f.Close()
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				_ = os.RemoveAll(vulnerableFolder)
+				_ = os.Mkdir(vulnerableFolder, os.ModePerm)
+				_ = junction.Create(maliciousFolder, vulnerableFolder)
+			}
+		}
+	}()
+	before := time.Now()
+	assert.Eventually(t, func() bool {
+		_, statErr := os.Lstat(fileInMaliciousPath)
+		return statErr == nil
+	}, time.Minute*1, time.Millisecond*100)
+
+	fmt.Printf("Malicious file detected after %d s", time.Since(before)/time.Second)
+	cancel()
+}
+
+func TestJunctionRaceAttackFail(t *testing.T) {
+	rootFolder := t.TempDir()
+	ctx, cancel := context.WithTimeout(t.Context(), time.Minute)
+	defer cancel()
+	const fileName = "file.txt"
+	maliciousFolder := filepath.Join(rootFolder, "malicious_folder")
+	vulnerableFolder := filepath.Join(rootFolder, "vulnerable_folder")
+	fileInCorrectPath := filepath.Join(vulnerableFolder, fileName)
+	fileInMaliciousPath := filepath.Join(maliciousFolder, fileName)
+
+	assert.NoError(t, os.Mkdir(maliciousFolder, os.ModePerm))
+
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				_ = os.RemoveAll(vulnerableFolder)
+				_ = os.Mkdir(vulnerableFolder, os.ModePerm)
+				// fd, _ := os.Create(fileInCorrectPath)
+				// _ = fd.Close()
+				fd, _, _ := CreateFile(fileInCorrectPath)
+				_ = windows.Close(fd)
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				_ = os.RemoveAll(vulnerableFolder)
+				_ = os.Mkdir(vulnerableFolder, os.ModePerm)
+				_ = junction.Create(maliciousFolder, vulnerableFolder)
+			}
+		}
+	}()
+	fileFound := atomic.Bool{}
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if _, statErr := os.Lstat(fileInMaliciousPath); statErr == nil {
+					cancel()
+					fileFound.Store(true)
+					return
+				}
+			}
+		}
+	}()
+	wg.Wait()
+	assert.False(t, fileFound.Load(), "The file in the malicious path should not be found")
+	cancel()
 }
--- go.mod
+++ go.mod
@@ -2,14 +2,17 @@ module SpikeBackup
 
 go 1.24.5
 
-require github.com/alecthomas/assert v1.0.0
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
 
 require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
-	github.com/alecthomas/colour v0.1.0 // indirect
-	github.com/alecthomas/repr v0.0.0-20210801044451-80ca428c5142 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/nyaosorg/go-windows-junction v0.2.0 // indirect
-	github.com/sergi/go-diff v1.2.0 // indirect
-	golang.org/x/sys v0.34.0 // indirect
+	github.com/nyaosorg/go-windows-junction v0.2.0
+	github.com/stretchr/testify v1.11.1
+	golang.org/x/sys v0.34.0
 )
